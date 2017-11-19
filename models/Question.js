5040de66d33cfdb8002a8a991cf1ae0e510f158d
jencekade <gaephuoc@gmail.com>
2017-11-19 00:54:49 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -1,34 +1,47 @@
 const db = require("../config/db")
 
-class Question{
+class Question {
   static CreateTable() {
-    return new Promise(function(resolve){
+    return new Promise(function(resolve) {
       const sql = `CREATE TABLE questions (
         id INTEGER PRIMARY KEY,
         content TEXT
       )`
-      
-      db.run(sql, function(){
+
+      db.run(sql, function() {
         resolve("questions table created")
-      })      
-    })
-  }
-
-  constructor(content){
-    this.content = content
-  }
-
-  insert(){
-    const self = this 
-    const sql = `INSERT INTO questions (content) VALUES (?)`
-    return new Promise(function(resolve){
-      db.run(sql, [self.content], function(err, result){
-        self.id = this.lastID
-        resolve(self)      
       })
     })
   }
 
+  constructor(content) {
+    this.content = content
+  }
+
+  insert() {
+    const self = this
+    const sql = `INSERT INTO questions (content) VALUES (?)`
+    return new Promise(function(resolve) {
+      db.run(sql, [self.content], function(err, result) {
+        self.id = this.lastID
+        resolve(self)
+      })
+    })
+  }
+
+  static Find(id) {
+    const sql = `SELECT * FROM questions WHERE id = ?`;
+    const self = this;
+    return new Promise(function(resolve) {
+      db.get(sql, [id], function(err, result) {
+        const question = new Question(result.content);
+        question.result = result.content;
+        question.id = result.id;
+        resolve(question)
+
+      });
+    });
+  }
 }
 
 module.exports = Question;
